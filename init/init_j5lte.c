0753631157740e043da5332f70a7382bb32f5df9
Nick Verse <nikoskalomiris@gmail.com>
2016-05-06 15:07:17 +0300

    Update init_j5lte.c

--- init/init_j5lte.c
+++ init/init_j5lte.c
@@ -69,22 +69,6 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.product.device", "j5lte");
 
         init_dsds();
-    } else if (strstr(bootloader, "J500M")) {
-       /* SM-J500M */
-       property_set("ro.product.model", "SM-J500M");
-       property_set("ro.build.fingerprint", "samsung/j5ltexx/j5lte:5.1.1/LMY48B/J500MUBU1APD1:user/release-keys");
-       property_set("ro.build.description", "j5ltexx-user 5.1.1 LMY48B J500MUBU1APD1 release-keys");
-       property_set("ro.product.device", "j5lte");
-
-       init_dsds();
-    } else if (strstr(bootloader, "J5008")) {
-       /* SM-J5008 */
-       property_set("ro.build.fingerprint", "samsung/j5ltexx/j5lte:5.1.1/LMY48B/J5008ZMU1AOL1:user/release-keys");
-       property_set("ro.build.description", "j5ltexx-user 5.1.1 LMY48B J5008ZMU1AOL1 release-keys");
-       property_set("ro.product.model", "SM-J5008");
-       property_set("ro.product.device", "j5lte");
-
-       init_dsds();
     }
 
     property_get("ro.product.device", device);
