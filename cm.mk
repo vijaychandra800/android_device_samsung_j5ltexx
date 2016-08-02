43963340d01a8ed801c3c48df5b6849c9f174d14
Nick Verse <nikoskalomiris@gmail.com>
2016-08-02 19:18:50 +0300

    Remove NFC for J500F (2)

--- cm.mk
+++ cm.mk
@@ -1,9 +1,6 @@
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 $(call inherit-product, device/samsung/j5ltexx/full_j5ltexx.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=j5ltexx TARGET_DEVICE=j5lte
