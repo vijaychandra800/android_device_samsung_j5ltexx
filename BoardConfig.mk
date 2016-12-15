f0455decd3de20867315af9f60ac2d342a18238b
nickverse <nikoskalomiris@gmail.com>
2016-12-15 22:58:16 +0200

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,60 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# Inherit from common j5
+-include device/samsung/j5-common/BoardConfigCommon.mk
+
+# Inherit from the proprietary version
+-include vendor/samsung/j5ltexx/BoardConfigVendor.mk
+
+# Audio
+AUDIO_FEATURE_SAMSUNG_DUAL_SIM := true
+
+# Radio
+SIM_COUNT := 2
+TARGET_GLOBAL_CFLAGS += -DANDROID_MULTI_SIM
+TARGET_GLOBAL_CPPFLAGS += -DANDROID_MULTI_SIM
+
+# Partition sizes
+BOARD_BOOTIMAGE_PARTITION_SIZE := 13631488
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 15728640
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2181038080
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 8388608
+BOARD_CACHEIMAGE_PARTITION_SIZE := 209715200
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 5016350720
+#(5016367104-16384= 5016350720 bytes)
+
+# Filesystem
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := j5lte
+
+# Kernel
+TARGET_KERNEL_CONFIG := cyanogenmod_j5nlte_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/j5lte
+
+# Vendor Init
+TARGET_UNIFIED_DEVICE := true
+TARGET_INIT_VENDOR_LIB := libinit_j5lte
+TARGET_RECOVERY_DEVICE_MODULES := libinit_j5lte
---
+++ README.md
@@ -0,0 +1,2 @@
+# android_device_samsung_j5ltexx
+Device tree for Samsung Galaxy J5 (DUOS)
