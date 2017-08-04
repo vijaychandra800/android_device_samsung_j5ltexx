fcb028578f60d4e669ac4bb48e3c6b1b622cd0e6
Vijay Chandra <vijaychandra800@gmail.com>
2017-08-04 14:58:52 +0530

    Update and rename cm.mk to lineage.mk

--- cm.mk
+++
@@ -1,8 +0,0 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-$(call inherit-product, device/samsung/j5ltexx/full_j5ltexx.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=j5ltexx TARGET_DEVICE=j5lte
-
-PRODUCT_NAME := cm_j5ltexx
---
+++ lineage.mk
@@ -0,0 +1,8 @@
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+$(call inherit-product, device/samsung/o7lte/full_o7lte.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=o7lte TARGET_DEVICE=o7lte
+
+PRODUCT_NAME := lineage_o7lte
