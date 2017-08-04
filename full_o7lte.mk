00c1e11d371de96bb540dbc8bb529771471f83d2
Vijay Chandra <vijaychandra800@gmail.com>
2017-08-04 15:01:36 +0530

    Update and rename full_j5ltexx.mk to full_o7lte.mk

--- full_o7lte.mk
+++ full_o7lte.mk
@@ -16,10 +16,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from j5ltexx device
-$(call inherit-product, device/samsung/j5ltexx/device.mk)
+$(call inherit-product, device/samsung/o7lte/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_j5ltexx
-PRODUCT_DEVICE := j5ltexx
+PRODUCT_NAME := full_o7lte
+PRODUCT_DEVICE := lineage_o7lte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
