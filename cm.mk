2a43042473251368c4d4df9367d23b5280dc910b
Vijay Chandra <vijaychandra800@gmail.com>
2017-08-04 15:57:02 +0530

    Update and rename lineage.mk to cm.mk

--- cm.mk
+++ cm.mk
@@ -5,4 +5,4 @@ $(call inherit-product, device/samsung/o7lte/full_o7lte.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=o7lte TARGET_DEVICE=o7lte
 
-PRODUCT_NAME := lineage_o7lte
+PRODUCT_NAME := cm_o7lte
