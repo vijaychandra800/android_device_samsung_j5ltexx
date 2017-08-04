7d716f4d46c6de6fef4cf0853c9dbcb9f5353a6b
Vijay Chandra <vijaychandra800@gmail.com>
2017-08-04 15:02:23 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,4 @@
-add_lunch_combo cm_j5ltexx-userdebug
-add_lunch_combo cm_j5ltexx-eng
+add_lunch_combo lineage_o7lte-userdebug
+add_lunch_combo lineage_o7lte-eng
+add_lunch_combo lineage_o7lte-user
+
